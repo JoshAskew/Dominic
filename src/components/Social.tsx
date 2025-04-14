6745bdadf3796b2bfa1ca53ef122d8fb65c75d87
JoshAskew <asksprit3@gmail.com>
2025-04-13 21:36:17 -0500

    done for the night

---
+++ src/components/Social.tsx
@@ -0,0 +1,13 @@
+import '../styles/Social.css';
+import React from 'react';
+
+const Social: React.FC = () => {
+  return (
+    <div>
+      <h1>Property Listings</h1>
+      <p>Explore our available properties.</p>
+    </div>
+  );
+};
+
+export default Social;
